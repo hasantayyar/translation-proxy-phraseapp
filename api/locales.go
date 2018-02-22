7663f726289f2c2d874fe3aa985dfccfd86230a7
theSoenke <theSoenke@users.noreply.github.com>
2018-02-22 21:31:22 +0100

    Store locales in cache

--- api/locales.go
+++ api/locales.go
@@ -1,12 +1,11 @@
 package api
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/allegro/bigcache"
+	"github.com/mitchellh/hashstructure"
 	"github.com/phrase/phraseapp-go/phraseapp"
 )
 
@@ -16,22 +15,43 @@ type locales struct {
 }
 
 func (l *locales) getLocale(projectID string, localeID string, params *phraseapp.LocaleDownloadParams) ([]byte, error) {
-	key := l.getCacheKey(localeID, params)
-	locale, err := l.Cache.Get(key)
-	if err != nil {
-		log.Printf("cache error: %s", err)
-	} else {
-		return locale, nil
+	key, err := l.getCacheKey(localeID, params)
+	if err == nil {
+		locale, err := l.Cache.Get(key)
+		if err != nil {
+			log.Printf("error: %s", err)
+		} else {
+			return locale, nil
+		}
 	}
 
-	locale, err = l.Client.LocaleDownload(projectID, localeID, params)
-	return locale, err
+	locale, err := l.Client.LocaleDownload(projectID, localeID, params)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := l.setLocale(localeID, locale, params); err != nil {
+		log.Println(err)
+	}
+
+	return locale, nil
 }
 
-func (l *locales) getCacheKey(localeID string, params *phraseapp.LocaleDownloadParams) string {
-	key := []byte(fmt.Sprintf("%s %v", localeID, params))
-	digest := md5.New()
-	digest.Write(key)
-	hash := digest.Sum(nil)
-	return hex.EncodeToString(hash)
+func (l *locales) setLocale(localeID string, locale []byte, params *phraseapp.LocaleDownloadParams) error {
+	key, err := l.getCacheKey(localeID, params)
+	if err != nil {
+		return err
+	}
+
+	err = l.Cache.Set(key, locale)
+	return err
+}
+
+func (l *locales) getCacheKey(localeID string, params *phraseapp.LocaleDownloadParams) (string, error) {
+	hash, err := hashstructure.Hash(params, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strconv.FormatUint(hash, 10), nil
 }
